66a892e49eda7ac31a608b3e5eed419a89c2635a
davlatbekxudoyqulov17-dotcom <davlatbekxudoyqulov17@gmail.com>
2026-09-24 20:54:19 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8969153580:AAEJqdG1rJVx6jCvTKngFk81T4s3zQUvfFk")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "8680762793"))
-DATABASE_URL = os.getenv("DATABASE_URL", "")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://baza_gy2l_user:UnbHi168PUaDGHRL0ZOYRBlR7Ti7aFK9@dpg-daqkenm0tbcc73fju9r0-a.oregon-postgres.render.com/baza_gy2l")
