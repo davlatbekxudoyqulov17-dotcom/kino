ae42ecc173f33adbf6abcf6a7698933063f9afc4
davlatbekxudoyqulov17-dotcom <davlatbekxudoyqulov17@gmail.com>
2026-09-24 20:47:50 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8969153580:AAEJqdG1rJVx6jCvTKngFk81T4s3zQUvfFk")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "8680762793"))
+DATABASE_URL = os.getenv("DATABASE_URL", "")
